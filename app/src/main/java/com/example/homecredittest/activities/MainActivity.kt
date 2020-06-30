73cac2394f41a2b0be81ed4540971125b2ac94f1
Rj Googliness <mendozaramonjeric@gmail.com>
2020-06-30 13:02:26 +0800

    + Remove Merlin library for Internet Observable

--- app/src/main/java/com/example/homecredittest/activities/MainActivity.kt
+++ app/src/main/java/com/example/homecredittest/activities/MainActivity.kt
@@ -1,15 +1,8 @@
 package com.example.homecredittest.activities
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import com.example.homecredittest.R
-import com.example.homecredittest.fragments.SecondFragment
-import com.novoda.merlin.Merlin
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
